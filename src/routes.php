b7ddaf9eb6421c35346e5051427064d5abea5b0c
AlefMach <alef_mac@hotmail.com>
2024-01-11 17:01:45 -0300

    created home page

--- src/routes.php
+++ src/routes.php
@@ -4,6 +4,7 @@ include './config/route.php';
 
 $router = new Router();
 
+$router->addRoute('', 'HomeController', 'index');
 $router->addRoute('products', 'ProductController', 'index');
 $router->addRoute('product/create', 'ProductController', 'create');
 $router->addRoute('product/save', 'ProductController', 'save');
